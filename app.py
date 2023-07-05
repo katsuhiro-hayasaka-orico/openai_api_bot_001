5a7b742da84efe7ebe427e542cee0fe629e41d3a
katsuhiro-hayasaka-orico <133623045+katsuhiro-hayasaka-orico@users.noreply.github.com>
2023-07-05 14:56:16 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-
 import streamlit as st
 import openai
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
-        ]
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
@@ -20,16 +19,27 @@ def communicate():
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages
-    )  
+        messages=messages,
+        stream=True  # ストリーミングオプションを有効にする
+    )
 
-    bot_message = response["choices"][0]["message"]
-    messages.append(bot_message)
+    result_area = st.empty()  # 結果を表示する空のエリアを作成
+    text = ''  # テキストの初期化
+
+    for event in response:
+        if event["type"] == "message":
+            bot_message = event["message"]["content"]
+            text += bot_message
+            result_area.write("🤖: " + text)  # テキストを更新して表示
+            
+            messages.append({"role": "assistant", "content": bot_message})
+
+            if "choices" in event and len(event["choices"]) > 0:
+                break
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
-
-# ユーザーインターフェイスの構築
+# ユーザーインターフェースの構築
 st.title("My AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
@@ -39,8 +49,5 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "🙂"
-        if message["role"]=="assistant":
-            speaker="🤖"
-
+        speaker = "🙂" if message["role"] == "user" else "🤖"
         st.write(speaker + ": " + message["content"])
